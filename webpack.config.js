bc16c720cbfcd43c6b5adce9e990f4fce06cc59f
alucard <devjrteste@gmail.com>
2021-08-31 01:06:45 -0300

    chore: configurando extenssoes, apagando pasta nodemodule e usando babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,18 @@ const path = require("path");
 module.exports = {
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: path.resolve(__dirname, "dist"),
-  filename: "bundle.js",
-};
+  filename: "bundle.js"
+},
+resolve: {
+  extensions: ['.js', 'jsx'],
+},
+
+module: {
+  rules: [
+    {
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }
+  ]
+}
