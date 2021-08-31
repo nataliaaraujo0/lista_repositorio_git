f974428c7a0bf0d6bc78397f1ae5d4488c1c0b98
alucard <devjrteste@gmail.com>
2021-08-31 01:22:46 -0300

    !refector: configurando output da forma correta

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,8 @@ const path = require("path");
 
 module.exports = {
   entry: path.resolve(__dirname, "src", "index.jsx"),
-  output: path.resolve(__dirname, "dist"),
-  filename: "bundle.js"
-},
-resolve: {
-  extensions: ['.js', 'jsx'],
-},
-
-module: {
-  rules: [
-    {
-      test: /\.jsx$/,
-      exclude: /node_modules/,
-      use: 'babel-loader'
-    }
-  ]
-}
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+};
