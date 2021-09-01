6cbf457b14f1758b77019891a553e98f8e37832c
alucard <devjrteste@gmail.com>
2021-08-31 23:04:15 -0300

    chore: config webpack dev server e source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devtool: "eval-source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -10,6 +11,10 @@ module.exports = {
   },
   resolve: {
     extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    port: 3000,
   },
   plugins: [
     new HtmlWebpackPlugin({
