044ecfe125f2c8566a64f3b06374e530d0877b03
alucard <devjrteste@gmail.com>
2021-09-13 00:01:40 -0300

    chore: criando componente RepositoryList

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,12 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Gallery-page-stake</h1>
+      <p>
+        Site feito para treinar responsividade com Media queries, flex-box e
+        grid system do bootstrap.
+      </p>
+      <a href="">Link do repositório</a>
+    </section>
+  );
+}
