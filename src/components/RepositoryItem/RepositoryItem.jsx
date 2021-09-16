9fde23f53f61c9bddc18042d82abd90d6634d8f6
alucard <devjrteste@gmail.com>
2021-09-16 01:23:24 -0300

    chore: refatorando component RepositoryList, criando outro component de itens

---
+++ src/components/RepositoryItem/RepositoryItem.jsx
@@ -0,0 +1,13 @@
+import "../../components/RepositoryItem/style.scss";
+export function RepositoryItem() {
+  return (
+    <div className="content-gallery">
+      <h1>Gallery-page-stake</h1>
+      <p>
+        Site feito para treinar responsividade com Media queries, flex-box e
+        grid system do bootstrap.
+      </p>
+      <button>Acessar</button>
+    </div>
+  );
+}
--- src/components/RepositoryList/RepositoryList.jsx
+++ src/components/RepositoryList/RepositoryList.jsx
@@ -1,61 +1,15 @@
 import "../../components/RepositoryList/style.scss";
+import { RepositoryItem } from "../RepositoryItem/RepositoryItem";
 export function RepositoryList() {
   return (
     <>
       <section className="container-gallery">
-        <div className="content-gallery">
-          <h1>Gallery-page-stake</h1>
-          <p>
-            Site feito para treinar responsividade com Media queries, flex-box e
-            grid system do bootstrap.
-          </p>
-          <button>Acessar</button>
-        </div>
-
-        <div className="content-gallery">
-          <h1>Gallery-page-stake</h1>
-          <p>
-            Site feito para treinar responsividade com Media queries, flex-box e
-            grid system do bootstrap.
-          </p>
-          <button>Acessar</button>
-        </div>
-
-        <div className="content-gallery">
-          <h1>Gallery-page-stake</h1>
-          <p>
-            Site feito para treinar responsividade com Media queries, flex-box e
-            grid system do bootstrap.
-          </p>
-          <button>Acessar</button>
-        </div>
-
-        <div className="content-gallery">
-          <h1>Gallery-page-stake</h1>
-          <p>
-            Site feito para treinar responsividade com Media queries, flex-box e
-            grid system do bootstrap.
-          </p>
-          <button>Acessar</button>
-        </div>
-
-        <div className="content-gallery">
-          <h1>Gallery-page-stake</h1>
-          <p>
-            Site feito para treinar responsividade com Media queries, flex-box e
-            grid system do bootstrap.
-          </p>
-          <button>Acessar</button>
-        </div>
-
-        <div className="content-gallery">
-          <h1>Gallery-page-stake</h1>
-          <p>
-            Site feito para treinar responsividade com Media queries, flex-box e
-            grid system do bootstrap.
-          </p>
-          <button>Acessar</button>
-        </div>
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
       </section>
     </>
   );
