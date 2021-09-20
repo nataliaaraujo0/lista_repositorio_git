fcca44a682403de4827efcb82fbd035c2b94bc8d
alucard <devjrteste@gmail.com>
2021-09-20 20:56:31 -0300

    chore: trazendo os repos da api do git

--- src/components/RepositoryItem/RepositoryItem.jsx
+++ src/components/RepositoryItem/RepositoryItem.jsx
@@ -4,8 +4,8 @@ export function RepositoryItem(props) {
   return (
     <li className="content-gallery">
       <strong>{props.repository.name}</strong>
-      <p>{props.repository.description}</p>
-      <a href="{props.repository.link}">Acessar</a>
+      <p>{props.repository.description ?? "Repositório Privado!"}</p>
+      <a href="{props.repository.html_url}">Acessar</a>
     </li>
   );
 }
