1e13e3c108a97547f6393211d3e67a253a6205c9
alucard <devjrteste@gmail.com>
2021-09-20 13:40:44 -0300

    chore: criando estado de increment e disparando no button

--- src/components/Counter/counter.jsx
+++ src/components/Counter/counter.jsx
@@ -1,10 +1,14 @@
 export function Counter() {
-    
+  function increment() {
+    console.log("Incrementing...");
+  }
 
   return (
     <div>
       <h2>0</h2>
-      <button type="button">Increment</button>
+      <button type="button" onclick={increment}>
+        Increment
+      </button>
     </div>
   );
 }
