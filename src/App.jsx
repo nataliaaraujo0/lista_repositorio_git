afa6b4a7e35aee383324dfaeebc8c2a06072a3ca
alucard <devjrteste@gmail.com>
2021-09-20 13:39:07 -0300

    chore: criando component de estado counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./styles/global.scss";
 import { Header } from "./components/Header/Header";
 import { RepositoryList } from "./components/RepositoryList/RepositoryList";
+import { Counter } from "./components/Counter/counter";
 
 export function App() {
   return (
-    <div>
+    <>
       <Header />
       <RepositoryList />
-    </div>
+      <Counter />
+    </>
   );
 }
---
+++ src/components/Counter/counter.jsx
@@ -0,0 +1,10 @@
+export function Counter() {
+    
+
+  return (
+    <div>
+      <h2>0</h2>
+      <button type="button">Increment</button>
+    </div>
+  );
+}
