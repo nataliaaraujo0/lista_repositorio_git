476f64097b28163ef97b11b7608aef24786134ba
alucard <devjrteste@gmail.com>
2021-09-20 14:15:55 -0300

    chore: adicioando Fast Refresh no Webpack

--- src/components/Counter/counter.jsx
+++ src/components/Counter/counter.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 export function Counter() {
+  const [counter, setCounter] = useState(0);
+
   function increment() {
-    console.log("Incrementing...");
+    setCounter(counter + 1);
   }
 
   return (
     <div>
-      <h2>0</h2>
-      <button type="button" onclick={increment}>
-        Increment
+      <h2>{counter}</h2>
+      <button type="button" onClick={increment}>
+        Increment +1
       </button>
     </div>
   );
