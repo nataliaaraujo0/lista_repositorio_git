86d7d983a293f7dfa82a0876c8f0d22679013db6
alucard <devjrteste@gmail.com>
2021-09-20 23:44:12 -0300

    chore: UI  e animações finalizadas

--- src/components/RepositoryItem/RepositoryItem.jsx
+++ src/components/RepositoryItem/RepositoryItem.jsx
@@ -4,8 +4,8 @@ export function RepositoryItem(props) {
   return (
     <li className="content-gallery">
       <strong>{props.repository.name}</strong>
-      <p>{props.repository.description ?? "Repositório Privado!"}</p>
-      <a href="{props.repository.html_url}">Acessar</a>
+      <p>{props.repository.description ?? "Sem descrição!"}</p>
+      <a href={props.repository.html_url}>Acessar</a>
     </li>
   );
 }
